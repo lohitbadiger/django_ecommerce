7924ca7559362dbaaa3157d357bc72abdb621917
Your Name <lohit2013n@gmail.com>
2019-12-30 10:39:08 +0530

    commit final

--- django_com/settings.py
+++ django_com/settings.py
@@ -82,13 +82,24 @@ WSGI_APPLICATION = 'django_com.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME':  'mydb',
+        'USER':'postgres',
+        'PASSWORD':'admin123',
+        'HOST':'localhost',
+        'PORT':'5433'
+        
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
